882de63610dbbac47bd4c90d10f5b15f6ed79805
anushhayrapetyan4-alt <anush.hayrapetyan4@edu.ysu.am>
2026-05-30 15:26:42 +0400

    Update load.py

--- load.py
+++ load.py
@@ -1,39 +1,13 @@
-import pandas as pd
-from sqlalchemy import text
+def load_to_database(df, table_name: str, engine) -> None:
+    """
+    Load dataframe into database.
+    """
 
-
-def load_to_mysql(categories_df, products_df, stats_df, engine):
-    print("3. Load: Լցնում ենք նոր բազայի մեջ...")
-
-    if categories_df.empty or products_df.empty:
-        return
-
-    categories_df.to_sql(
-        'categories',
+    df.to_sql(
+        table_name,
         con=engine,
-        if_exists='append',
+        if_exists="replace",
         index=False
     )
 
-    with engine.connect() as conn:
-        cat_id = conn.execute(
-            text("SELECT LAST_INSERT_ID()")
-        ).fetchone()[0]
-
-    products_df['category_id'] = cat_id
-
-    products_df.to_sql(
-        'products',
-        con=engine,
-        if_exists='append',
-        index=False
-    )
-
-    stats_df.to_sql(
-        'product_stats',
-        con=engine,
-        if_exists='append',
-        index=False
-    )
-
-    print("--- Տվյալները հաջողությամբ փոխանցվեցին! ---\n")
+    print(f"{table_name} loaded successfully.")
