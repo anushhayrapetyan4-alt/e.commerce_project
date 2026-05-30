9fa6b8acd463e5ff57337bdff4f598137ce27f6f
anushhayrapetyan4-alt <anush.hayrapetyan4@edu.ysu.am>
2026-05-30 15:30:25 +0400

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,16 +11,22 @@ CREATE TABLE categories (
 CREATE TABLE products (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(500) NOT NULL,
-    category_id INT,
+    category_id INT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
 CREATE TABLE product_stats (
     stat_id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT,
-    original_price DECIMAL(10, 2),
-    sale_price DECIMAL(10, 2),
+    product_id INT NOT NULL,
+    original_price DECIMAL(10,2),
+    sale_price DECIMAL(10,2),
     discount_rate INT,
-    review_count INT,
-    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+    review_count INT DEFAULT 0,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+CREATE INDEX idx_category
+ON products(category_id);
+
+CREATE INDEX idx_product
+ON product_stats(product_id);
